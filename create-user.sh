a31dce3d27b6e80bf3445783db6c53d05753640c
Vasu Sheoran <gokusayon@gmail.com>
2020-07-10 02:27:51 +0530

    Added promt for username.

--- create-user.sh
+++ create-user.sh
@@ -1,29 +1,12 @@
 # Set up new user
-echo "Creating new user vasu"
+echo "Enter username: "
 
-default_user=vasu
-USERS="$default_user"
+read default_user
 
-for i in $USERS; do
-    case `id -u $i > /dev/null 2>&1; echo $?` in
-        0)
-            # user found
-            echo "user '$i' found. Skipping .."
-            ;;
-        1)
-            # user not found
-            echo "Adding user '$i' with root privilages"
-            adduser $default_user
-            usermod -aG sudo $default_user
-            su - $default_user
-            sudo ls -la /root
-            ;;
-        *)
-            # code if an error occurred
-            echo "Error finding go path. Please add manually"
-            ;;
-    esac
-done
+adduser $default_user
+usermod -aG sudo $default_user
+su - $default_user
+sudo ls -la /root
 
 # Check
 sudo -i -u vasu bash << EOF
