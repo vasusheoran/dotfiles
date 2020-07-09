6a61733a03a0e9f9be708d96d44095c50b905bca
Vasu Sheoran <gokusayon@gmail.com>
2020-07-10 05:26:34 +0530

    Fixed minor bugs.

--- install.sh
+++ install.sh
@@ -25,29 +25,30 @@ git clone https://github.com/vasusheoran/dotfiles.git
 cp ~/dotfiles/shell/.gitconfig.user ~/.gitconfig.user
 
 # Installing oh-my-zsh
-echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+[ -d ~/.oh-my-zsh ] || echo "Installing oh-my-zsh"
+[ -d ~/.oh-my-zsh ] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 sudo chsh -s $(which zsh) $(whoami)
 
-echo "Installing Powerlevel10k theme"
-[[ ! -f ~/.oh-my-zsh/custom/themes/powerlevel10k ]] || git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+[ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ] || git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+[ ! -d ~/.oh-my-zsh ] || echo "Powerlevel10k exists. Skipping ..."
 
 # Install zsh-plugins
-[[ ! -f ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-[[ ! -f ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]] || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+[ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+[ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 # Install FZF
-[[ ! -f ~/.fzf ]] || git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+echo "Installing fzf."
+[ -d ~/.fzf ] || git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
+echo "Creating links..."
 # Add links to symbolic files config files  
 ln -sf ~/dotfiles/shell/.alias ~/.alias \
-  && ln -sf ~/dotfiles/shell/.bashrc ~/.bashrc \
   && ln -sf ~/dotfiles/shell/.profile ~/.profile \
-  && ln -sf ~/dotfiles/shell/.zshrc ~/.zshrc \
   && ln -sf ~/dotfiles/shell/.p10k.zsh ~/.p10k.zsh \
   && sudo ln -sf ~/dotfiles/etc/.wslconf /etc/wsl.conf \
-  && ln -s ~/dotfiles/.gitconfig ~/.gitconfig \
-  # && ln -sf ~/dotfiles/shell/.tmux.conf ~/.tmux.conf \
+  && ln -sf ~/dotfiles/.gitconfig ~/.gitconfig \
+  && ln -sf ~/dotfiles/shell/.zshrc ~/.zshrc \
 
-[[ ! -f ~/.zshrc ]] || source ~/.zshrc
+echo "\nFinished setup.\nLogin to shell again to apply the changes or use the command below.\n\n #   source ~/.zshrc"
---
+++ remove.sh
@@ -0,0 +1,5 @@
+sudo rm -r ~/.oh-my-zsh/
+sudo rm -r ~/.fzf*
+sudo rm -r ~/.z*
+sudo rm -r ~/.p10*
+sudo rm -r ~/.alias
