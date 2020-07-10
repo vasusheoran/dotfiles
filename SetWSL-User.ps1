1853e4dce4e63e6b21b875abd2f243a416c2e64d
Vasu Sheoran <gokusayon@gmail.com>
2020-07-10 21:54:50 +0530

    Fixed fzf input and added prompt to ps scipt.

--- SetWSL-User.ps1
+++ SetWSL-User.ps1
@@ -1,4 +1,4 @@
-Function WSL-SetDefaultUser ($distro="Ubuntu", $user="vasu") 
+Function WSL-SetDefaultUser ($distro, $user) 
 {
     echo "Setting User : $user, distro : $distro" 
     Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq $distro | Set-ItemProperty -Name DefaultUid -Value ((wsl -d $distro -u $user -e id -u) | Out-String); 
@@ -13,5 +13,8 @@ Function GetUserID ($distro="Ubuntu", $user="vasu")
 
 };
 
-WSL-SetDefaultUser -distro Ubuntu-20.04 -user vasu;
+$distro = Read-Host -Prompt 'Input your distro name'
+$user = Read-Host -Prompt 'Input your user name'
+
+WSL-SetDefaultUser -distro $distro -user $user;
 #GetUserID -distro Ubuntu -user vasu;
