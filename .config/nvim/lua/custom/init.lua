84b474e3583786435333d08dd39dbc812e845171
Vasu Sheoran <vsheoran@salesforce.com>
2024-12-30 17:01:33 +0530

    initial commit with stow

--- .config/nvim/lua/custom/init.lua
+++
@@ -1,9 +0,0 @@
--- vim.api.nvim_create_autocmd("BufWinEnter", {
---         pattern = "*.templ",
---         command = "set filetype=templ",
--- })
-
--- Add .templ extension to filetype
-vim.filetype.add({ extension = { templ = "templ" } })
-
-vim.wo.relativenumber = true
