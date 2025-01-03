cc210028d5eec7a2258a7f8b732ae80a530ba102
Vasu Sheoran <vasusheoran92@gmail.com>
2025-01-03 12:44:47 +0000

    kickstarter.nvim

---
+++ .catpuccin.lua
@@ -0,0 +1,9 @@
+{
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "catppuccin-mocha"
+  end
+}
--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -3,70 +3,35 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			vim.keymap.set('n', '<C-n>', '<cmd> Neotree filesystem reveal left toggle<CR>', {})
-		end
-	},
-	{
-		'akinsho/bufferline.nvim',
-		version = "*",
-		dependencies = 'nvim-tree/nvim-web-devicons',
-		config = function()
-			require("bufferline").setup {}
-		end
-	},
-	{
-		"mbbill/undotree",
+    {
+        "goolord/alpha-nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
 
-	},
-	{
-		"tpope/vim-fugitive",
-		config = function()
-			local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.startify")
 
-			local autocmd = vim.api.nvim_create_autocmd
-			autocmd("BufWinEnter", {
-				group = ThePrimeagen_Fugitive,
-				pattern = "*",
-				callback = function()
-					if vim.bo.ft ~= "fugitive" then
-						return
-					end
+            dashboard.section.header.val = {
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                     ]],
+            [[       ████ ██████           █████      ██                     ]],
+            [[      ███████████             █████                             ]],
+            [[      █████████ ███████████████████ ███   ███████████   ]],
+            [[     █████████  ███    █████████████ █████ ██████████████   ]],
+            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            }
 
-					local bufnr = vim.api.nvim_get_current_buf()
-					local opts = { buffer = bufnr, remap = false }
-					vim.keymap.set("n", "<leader>p", function()
-						vim.cmd.Git('push')
-					end, opts)
-
-					-- rebase always
-					vim.keymap.set("n", "<leader>P", function()
-						vim.cmd.Git({ 'pull', '--rebase' })
-					end, opts)
-
-					-- NOTE: It allows me to easily set the branch i am pushing and any tracking
-					-- needed if i did not set the branch up correctly
-					vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
-				end,
-			})
-		end
-	},
-	{
-		"theprimeagen/harpoon",
-		config = function()
-			local mark = require("harpoon.mark")
-			local ui = require("harpoon.ui")
-
-			vim.keymap.set("n", "<leader>m", mark.add_file)
-			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-		end
-	}
+            alpha.setup(dashboard.opts)
+        end,
+    }
 }
