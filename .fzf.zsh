1ae3a3504fda589193565b2870e1193fab0e565d
Vasu Sheoran <vsheoran@salesforce.com>
2025-06-23 11:14:12 +0530

    stable nvim config

--- .fzf.zsh
+++ .fzf.zsh
@@ -11,3 +11,11 @@ source "/opt/homebrew/opt/fzf/shell/completion.zsh"
 # Key bindings
 # ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+if [ -f ~/.alias ]; then
+    source ~/.alias
+fi
+
+if [ -f ~/.alias.sf ]; then
+    source ~/.alias.sf
+fi
