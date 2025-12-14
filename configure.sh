201939b432e5b4a09006e7b4f0fe3baf66f717d2
Vasu Sheoran <vasusheoran92@gmail.com>
2025-12-15 02:55:49 +0530

    fix: swaync and configure.sh

--- configure.sh
+++ configure.sh
@@ -2,15 +2,15 @@
 
 # Configuration script to deploy dotfiles using 'stow'
 
-PACKAGES="backgrounds hyprland kitty nvim rofi swaync waybar wlogout"
+PACKAGES="backgrounds hyprland hyprpaper hyprlock hyprmocha kitty nvim rofi swaync waybar wlogout"
 STOW_ROOT="$HOME/dotfiles"
 
 echo "--- Starting Dotfiles Configuration (stow) ---"
 cd "$STOW_ROOT" || { echo "Error: Cannot navigate to $STOW_ROOT. Aborting."; exit 1; }
 
 for pkg in $PACKAGES; do    
-    mv "$pkg" "$pkg.bak" 2>/dev/null
-    mkdir -p "$HOME/.config/$pkg"
+    mv "$HOME/.config/$pkg" "$HOME/.config/$pkg.bak" 2>/dev/null
+    mkdir -p "$HOME/.config/$pkg"/
 done
 
 echo "Deploying packages: $PACKAGES"
